61221a3a94d6d171a257e778b2cee89966cfbfc8
Ming Lu <mclu@asicentral.com>
2020-01-22 10:30:54 -0500

    create storage.

---
+++ azure/deploy.ps1
@@ -0,0 +1,20 @@
+$resourceGroup = "sample"
+$location = "East US"
+$templateFile = ".\azuredeploy.json"
+
+
+Remove-AzResourceGroup -Name sample -Force
+
+Get-AzResourceGroup -Name $resourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if($notPresent)
+{
+    New-AzResourceGroup -Name $resourceGroup -Location $location
+}
+    
+New-AzResourceGroupDeployment `
+  -Name sampleDeployment2 `
+  -ResourceGroupName $resourceGroup `
+  -TemplateFile $templateFile `
+  -storagePrefix "asiming" `
+  -storageSKU "Standard_LRS"
