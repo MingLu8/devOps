bd30db5205b8595cc125d701aa389a4eede8ed53
Ming Lu <mclu@asicentral.com>
2020-01-22 14:42:31 -0500

    added keyvault

--- azure/deploy.ps1
+++ azure/deploy.ps1
@@ -1,20 +1,31 @@
 $resourceGroup = "sample"
 $location = "East US"
 $templateFile = ".\azuredeploy.json"
+$templateParameterFile = ".\azuredeploy.parameters.json"
 
 
 # Remove-AzResourceGroup -Name sample -Force
 
 Get-AzResourceGroup -Name $resourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
 
-if($notPresent)
-{
+if($notPresent) {
     New-AzResourceGroup -Name $resourceGroup -Location $location
 }
     
+# Set-AzKeyVaultAccessPolicy -VaultName ExampleVault -EnabledForTemplateDeployment
+
+# Get-AzKeyVault -VaultName sample -ResourceGroupName sample -ErrorVariable noVaultExists
+# if($noVaultExists) {
+#     New-AzKeyVault -VaultName ExampleVault -resourceGroupName $resourceGroup -Location $location -EnabledForTemplateDeployment
+# }
+    
+# $secretvalue = ConvertTo-SecureString 'hVFkk965BuUv' -AsPlainText -Force
+# $secret = Set-AzKeyVaultSecret -VaultName ExampleVault -Name 'ExamplePassword' -SecretValue $secretvalue
+
 New-AzResourceGroupDeployment `
   -Name sampleDeployment3 `
   -ResourceGroupName $resourceGroup `
   -TemplateFile $templateFile `
+  -TemplateParameterFile $templateParameterFile `
   -storagePrefix "asiming" `
   -storageSKU "Standard_LRS"
