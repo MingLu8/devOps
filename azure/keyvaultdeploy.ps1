d998fd066ba1480d0533f8eedfac48ef83bc5df6
ming <ming.lu@live.com>
2020-01-22 23:51:07 -0500

    working keyvault sample

--- azure/keyvaultdeploy.ps1
+++ azure/keyvaultdeploy.ps1
@@ -1,15 +1,15 @@
-$resourceGroup = "sample"
+$resourceGroup = "sample7"
 $location = "East US"
 $templateFile = ".\keyvaultdeploy.json"
 $templateParameterFile = ".\keyvaultdeploy.parameters.json"
+$keyVaultName = "SomeKeyVault7"
 
-
-# Remove-AzResourceGroup -Name sample -Force
+#Remove-AzResourceGroup -Name sample -Force
 
 Get-AzResourceGroup -Name $resourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
 
-if($notPresent) {
-    New-AzResourceGroup -Name $resourceGroup -Location $location
+if ($notPresent) {
+  New-AzResourceGroup -Name $resourceGroup -Location $location
 }
     
 # Set-AzKeyVaultAccessPolicy -VaultName ExampleVault -EnabledForTemplateDeployment
@@ -21,10 +21,43 @@ if($notPresent) {
     
 # $secretvalue = ConvertTo-SecureString 'hVFkk965BuUv' -AsPlainText -Force
 # $secret = Set-AzKeyVaultSecret -VaultName ExampleVault -Name 'ExamplePassword' -SecretValue $secretvalue
+function GetKeyVaultAccessPolicy([string][parameter(Mandatory = $true)] $keyVaultName) {
 
+  $keyVaultAccessPolicies = (Get-AzKeyVault -VaultName $keyVaultName).accessPolicies
+  $armAccessPolicies = @()
+
+  if ($keyVaultAccessPolicies) {
+    foreach ($keyVaultAccessPolicy in $keyVaultAccessPolicies) {
+      $armAccessPolicy = [pscustomobject]@{
+        tenantId = $keyVaultAccessPolicy.TenantId
+        objectId = $keyVaultAccessPolicy.ObjectId
+      }
+
+      $armAccessPolicyPermissions = [pscustomobject]@{
+        keys         = $keyVaultAccessPolicy.PermissionsToKeys
+        secrets      = $keyVaultAccessPolicy.PermissionsToSecrets
+        certificates = $keyVaultAccessPolicy.PermissionsToCertificates
+        storage      = $keyVaultAccessPolicy.PermissionsToStorage
+      }
+
+      $armAccessPolicy | Add-Member -MemberType NoteProperty -Name permissions -Value $armAccessPolicyPermissions
+
+      $armAccessPolicies += $armAccessPolicy
+    }
+  }
+
+  $armAccessPoliciesParameter = $armAccessPolicies
+
+  $armAccessPoliciesParameter = $armAccessPoliciesParameter | ConvertTo-Json -Depth 5 -Compress
+
+  #Write-Host ("##vso[task.setvariable variable=Infra.KeyVault.AccessPolicies;]$armAccessPoliciesParameter")
+  return $armAccessPoliciesParameter
+}
+@(GetKeyVaultAccessPolicy $keyVaultName)
 New-AzResourceGroupDeployment `
-  -Name keyvaultDeploy `
+  -Name keyvaultDeploy3 `
   -ResourceGroupName $resourceGroup `
   -TemplateFile $templateFile `
   -TemplateParameterFile $templateParameterFile `
+  -keyVaultName $keyVaultName `
   -Debug
