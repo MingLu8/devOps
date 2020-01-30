ec79f46a9596a71727db3baa8ecb6e2a8cd01121
Ming Lu <mclu@asicentral.com>
2020-01-30 11:53:17 -0500

    working copy of webp function and cdn.

--- azure/templates/create-cdn-endpoint.ps1
+++ azure/templates/create-cdn-endpoint.ps1
@@ -1,12 +1,14 @@
 $userPrinciple = Get-AzADUser -UserPrincipalName mclu@asicentral.com | Select-Object -ExpandProperty Id
 $functionPackageUri = "https://asideploymentstorage.blob.core.windows.net/deployments/webp/webp/publish.zip?sp=r&st=2020-01-30T05:48:32Z&se=2021-01-30T13:48:32Z&spr=https&sv=2019-02-02&sr=b&sig=lPpnWIQmOEhU9Voh4KRt0xZROcWjJRtIprIs9jd42As%3D"
-$resourceGroup = "deploy-webp-group-ming13"
+$resourceGroup = "deploy-webp-group-ming11"
 $location = "East US"
-$templateFile = ".\deploy.json"
+$templateFile = ".\create-cdn-endpoint.json"
 #$templateParameterFile = ".\keyvaultdeploy.parameters.json"
 #$keyVaultName = "webpsamplevault"
 $templateBaseUrl = "https://raw.githubusercontent.com/MingLu8/devOps/master/azure"
-
+$cdnEndpointName = "samplecdn"
+$cdnEndPointOrigin = "func-app-v6vxfwotr3rzi.azurewebsites.net"
+$functionKey = "sadfasdf"                                    
 #Remove-AzResourceGroup -Name sample -Force
 
 Get-AzResourceGroup -Name $resourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue
@@ -19,7 +21,7 @@ New-AzResourceGroupDeployment `
     -Name createDeployment3 `
     -ResourceGroupName $resourceGroup `
     -TemplateFile $templateFile `
-    -templateBaseUrl $templateBaseUrl `
-    -userPrincipleId $userPrinciple `
-    -functionPackageUri $functionPackageUri `
-    # -debug
+    -cdnEndPointOrigin $cdnEndPointOrigin `
+    -functionKey $functionKey `
+    -cdnEndpointName $cdnEndpointName `
+    -debug
